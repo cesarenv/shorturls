b90100b7b83d37a91f7983dbafbe8f8b6af36a4c
Cesar <cesarferradas@pm.me>
2020-04-19 18:21:59 +0100

    remove User schema and controllers

--- models/index.js
+++ models/index.js
@@ -1,7 +1,5 @@
-const User = require('./user')
 const Link = require('./link')
 
 module.exports = {
-  User,
   Link,
 }
--- settings.js
+++ settings.js
@@ -1,17 +1,7 @@
-const winston = require('winston')
-
 const port = process.env.PORT || 4000
-const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/urlshort'
-
-const logger = winston.createLogger({
-  format: winston.format.simple(),
-  transports: [
-    new winston.transports.Console(),
-  ],
-})
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/shorturls'
 
 module.exports = {
   databaseUrl,
   port,
-  logger,
 }
