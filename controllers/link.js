257f3b31b18f4177652f3f213a5010d74f65a03f
cesar <dev@cesar.sh>
2020-04-19 23:41:54 +0100

    move logger to utils directory

--- controllers/link.js
+++ controllers/link.js
@@ -1,10 +1,13 @@
 const models = require('../models')
+const logger = require('../utils/logger')
+
 
 const create = (req, res) => {
   // TODO handle shortid collisions
   const newLink = new models.Link(req.body)
   newLink.save((err, link) => {
     if (err) {
+      logger.error(`could not create Link: ${err}`)
       res.status(400).json({
         status: 400,
         message: err.message,
@@ -20,12 +23,9 @@ const create = (req, res) => {
 
 const redirect = (req, res) => {
   models.Link.findById(req.params.linkId, (err, link) => {
-    if (err) {
-      // TODO redirect to somewhere more meaningful
-      res.status(400).json({
-        status: 400,
-        message: err.message,
-      })
+    // TODO redirect to somewhere more meaningful on error
+    if (err || !link) {
+      res.status(404).send('Not found')
     } else {
       res.redirect(link.url)
     }
