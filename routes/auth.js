c7ef4785a148d38f1c5c8b00a0b2fd30d80a9c22
Cesar <cesarferradas@pm.me>
2020-04-21 10:16:18 +0100

    add User model and JWT auth

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const user = require('./controllers/user')
+
+const auth = express.Router()
+
+auth.route('/register')
+  .post(user.register)
+
+auth.route('/login')
+  .post(user.login)
+
+module.exports = auth
--- routes/controllers/link.js
+++ routes/controllers/link.js
@@ -1,13 +1,10 @@
-const models = require('../models')
-const logger = require('../utils/logger')
-
+const Link = require('../../models/link')
 
 const create = (req, res) => {
   // TODO handle shortid collisions
-  const newLink = new models.Link(req.body)
+  const newLink = new Link(req.body)
   newLink.save((err, link) => {
     if (err) {
-      logger.error(`could not create Link: ${err}`)
       res.status(400).json({
         status: 400,
         message: err.message,
@@ -22,7 +19,7 @@ const create = (req, res) => {
 }
 
 const redirect = (req, res) => {
-  models.Link.findById(req.params.linkId, (err, link) => {
+  Link.findById(req.params.linkId, (err, link) => {
     // TODO redirect to somewhere more meaningful on error
     if (err || !link) {
       res.status(404).send('Not found')
