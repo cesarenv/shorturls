9d4ff25f3a45c717013178ffeb699241ed93e401
cesar <dev@cesar.sh>
2020-04-21 10:16:18 +0100

    add User model and JWT auth

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const helmet = require('helmet')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const winston = require('winston')
 
 const api = require('./routes/api')
+const auth = require('./routes/auth')
 const base = require('./routes/base')
-const logger = require('./utils/logger')
 
 const app = express()
 
 const port = process.env.PORT
+
+const logger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+})
 
 mongoose.connect(`mongodb://${process.env.DB_HOST}/shorturls`, {
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
@@ -19,12 +26,30 @@ mongoose.connect(`mongodb://${process.env.DB_HOST}/shorturls`, {
   socketTimeoutMS: 10000,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).catch((err) => logger.error(err))
+}).catch(() => logger.error('Could not connect to MongoDB'))
 
 app.use(bodyParser.json())
 app.use(helmet())
-app.use(morgan('tiny'))
+app.use(morgan('combined'))
 
+// Authentication
+app.use((req, res, next) => {
+  req.user = undefined
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET, (err, decode) => {
+      if (err || !decode) {
+        next()
+      } else {
+        req.user = decode
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+app.use('/api/auth/', auth)
 app.use('/api/', api)
 app.use('/', base)
 
--- models/index.js
+++
@@ -1,5 +0,0 @@
-const Link = require('./link')
-
-module.exports = {
-  Link,
-}
