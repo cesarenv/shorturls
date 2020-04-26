bb3853bc2d1fd8661505bdc6102ef799a51a0fb6
Cesar <cesarferradas@pm.me>
2020-04-26 20:45:09 +0100

    add defaults to config.js

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ config.jwt = {
 config.linkSize = 7
 
 config.mongo = {
-  host: process.env.MONGO_HOST || 'localhost',
+  host: process.env.MONGO_HOST,
   name: process.env.MONGO_NAME || 'shorturls',
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
   password: process.env.MONGO_INITDB_ROOT_PASSWORD,
@@ -23,10 +23,10 @@ config.morgan = {
 
 config.port = process.env.PORT || 3000
 
-config.rateLimitTTL = process.env.RATE_LIMIT_TTL || 1
+config.rateLimitTTL = process.env.RATE_LIMIT_TTL || 0
 
 config.redis = {
-  host: process.env.REDIS_HOST || 'localhost',
+  host: process.env.REDIS_HOST,
 }
 
 config.saltRounds = 10
